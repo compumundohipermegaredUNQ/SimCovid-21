815516c273974149e2caff18c2034016265929c3
Juan Cruz Vincenti <juancruzvincenti@gmail.com>
2021-06-17 21:18:31 -0300

    Agregadas burbujas de texto

--- src/City/Pedestrians/Pedestrian.gd
+++ src/City/Pedestrians/Pedestrian.gd
@@ -7,25 +7,34 @@ var animations
 var direction = 1
 var percentage
 var pedestrian_bar_name
+var bubble_speech
+var bubble_speech_label
 
 func initialize(bar_percentage, bar_name):
 	rng = RandomNumberGenerator.new()
 	animations = ["male_walk", "female_walk"]
 	percentage = bar_percentage
 	pedestrian_bar_name = bar_name
+	bubble_speech = $BubbleSpeech
+	bubble_speech_label = $BubbleSpeech/Label
+	bubble_speech.hide()
 
 func _move_to_right():
 	direction = 1
 	self.flip_h = 0
+	bubble_speech.flip_h = 0
+	bubble_speech.offset = Vector2(0,0)
 
 func _move_to_left():
 	direction = -1
 	self.flip_h = 1
+	bubble_speech.flip_h = 1
+	bubble_speech.offset = Vector2(-400, 0)
+	bubble_speech_label.set_position(Vector2(-739, -170))
 
 func _process(delta):
 	position.x += direction * speed * delta
 	
-
 func set_male_animation():
 	self.play("male_walk")
 
@@ -61,3 +70,17 @@ func set_random_animation():
 	else:
 		self.play(gender)
 	print(pedestrian+'_'+gender)
+
+func set_random_phrase():
+	rng.randomize()
+	if PedestrianDatabase.get_current_ticks() == 0:
+		bubble_speech.show()
+		var phrase = PedestrianDatabase.PHRASES[randi() % PedestrianDatabase.PHRASES.size()]
+		if '{user_name}' in phrase:
+			phrase = phrase.replace('{user_name}', PedestrianDatabase.get_user_name())
+		bubble_speech_label.text = phrase
+		PedestrianDatabase.restart_ticks()
+
+
+
+
--- src/City/Pedestrians/PedestrianDatabase.gd
+++ src/City/Pedestrians/PedestrianDatabase.gd
@@ -1,5 +1,8 @@
 extends Node
 
+var ticks_per_phrase = 10
+var user_name
+
 const PEDESTRIANS = {
 	"low": {
 		"cultural_bar": "neanderthal",
@@ -14,3 +17,33 @@ const PEDESTRIANS = {
 		"social_bar": "worker"
 	}
 }
+
+const PHRASES = [
+	"{user_name} me aumentó el monotributo, RETROACTIVO",
+	"Menos mal que tenemos menos pobres que Alemania",
+	"Que lujo es nuestra economía. Dijo nunca nadie",
+	"{user_name} dijo que si deposité dolares, voy a recibir dolares",
+	"{user_name} me quiere encerrar de nuevo",
+	"Hay demasiada gente en la calle. Vuelvan a sus casas!",
+	"{user_name} dijo que los de Brasil salieron de la selva",
+	"No aguanto más a mis hijos en casa",
+	"{user_name}, abrime las escuelas!",
+	"Quiero ver rápidos y furiosos 9 en el cine!",
+	"{user_name}, habilitame los bares!",
+	"Si me doy la rusa seguro termino hecho un robot",
+	"Ese señor no tiene barbijo!",
+	"Esperemos que no se relaje el sistema sanitario"
+]
+
+func set_user_name(name):
+	user_name = "El gato" if name == '' || name == null else name
+
+func get_user_name():
+	return user_name
+
+func get_current_ticks():
+	ticks_per_phrase = clamp(ticks_per_phrase - 1, 0, 10)
+	return ticks_per_phrase
+
+func restart_ticks():
+	ticks_per_phrase = 10
